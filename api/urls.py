0b597a3a5d9b1f454f3c4b615437ace9af3f262f
Sergio Liberman B <sliberman@alges.cl>
2018-08-27 16:17:19 +0200

    Fixed urls issues

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,10 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('store/data_files/<str:data_file>/<str:formatter>', views.StoreDataView.as_view(), name="store_data_files"),
-    path('get/data_files', views.GetDataView.as_view(), name="get_data_files"),
-    path('store/labels_files/<str:formatter>', views.StoreLabelsView.as_view(), name="store_labels_files"),
-    path('get/labels_files', views.GetLabelsView.as_view(), name="get_labels_files"),
-
+    path('store/input_data/<str:data_file>/<str:formatter>', views.StoreDataView.as_view(), name="store_data_files"),
+    path('get/input_data', views.GetDataView.as_view(), name="get_data_files"),
+    path('store/input_labels/<str:formatter>', views.StoreLabelsView.as_view(), name="store_labels_files"),
+    path('get/input_labels', views.GetLabelsView.as_view(), name="get_labels_files"),
     path('get/<str:scope>', views.GetScopeOptionsView.as_view()),
     path('user_pipelines', views.UsersPipelinesView.as_view()),
     path('user_pipelines/create', views.UsersPipelinesCreateView.as_view()),
