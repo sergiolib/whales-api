efeb1f94cc41b91dcaefad01e03b4e8874c24a95
Sergio Liberman B <sliberman@alges.cl>
2018-08-30 01:12:04 +0200

    Added files management to admin

--- api/admin.py
+++ api/admin.py
@@ -6,4 +6,6 @@ from django.contrib import admin
 admin.site.register(Permission)
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Pipeline)
+admin.site.register(models.DataFile)
+admin.site.register(models.LabelsFile)
 admin.site.register(models.LaunchPipelineTask)
